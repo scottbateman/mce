8b909291b9ab8179c7aaa0f4776aa1dd5587bbf2
Scott Bateman <sbateman@upei.ca>
2014-07-18 15:45:40 -0300

    figuring out how to use share.js

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,9 +1,10 @@
-requirejs.config({
+  requirejs.config({
    baseUrl: '/js/lib'
    , paths: {
       "ace": "ace/lib/ace"
       , "bcsocket": "/channel/bcsocket"
       , "shareJS": "sharejs/share"
+      , "shareJSON": "sharejs/json"
       , "sharejs_ace": "sharejs/ace"
       , "ace_java": "ace/mode/java"
       , "jquery": "jquery/jquery-2.1.1.min"
@@ -19,19 +20,33 @@ requirejs.config({
       , "sharejs_ace": {
          deps: ["ace/ace", "shareJS"]
       }
+      , "shareJSON": {
+         deps: ["shareJS"]
+      }
       , "ace_java": {
          deps: ['ace/ace']
       }
    }
 });
 
-requirejs(['ace/ace', 'shareJS', 'bcsocket', 'sharejs_ace','jquery'],
+var $state;
+var $localState;
+
+function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
+
+requirejs(['ace/ace', 'shareJS', 'bcsocket', 'sharejs_ace','shareJSON','jquery'],
    function(ace, sharejs) {
       var editor = ace.edit('editor');
       editor.setTheme('ace/theme/twilight');
       editor.getSession().setMode('ace/mode/java');
 
-      sharejs.open('editor', 'text', function(error, doc) {
+      
+
+      /*sharejs.open('editor', 'text', function(error, doc) {
           var id;
           xmlhttp = new XMLHttpRequest();
           xmlhttp.open("GET","http://localhost:8950/user", true);
@@ -63,27 +78,48 @@ requirejs(['ace/ace', 'shareJS', 'bcsocket', 'sharejs_ace','jquery'],
 
 
          doc.attach_ace(editor);
-      });
-       var dataDoc = {};
+      });*/
 
        $('#editor').keyup(function(){
            var cursorSpot = $('.ace_cursor').position();
            var x = cursorSpot.left + 79;
            var y = cursorSpot.top + 103;
-           dataDoc.x = x;
-           dataDoc.y = y;
-           console.log(x+"--"+y);
-       });
-       sharejs.open('dataDoc','text',function(error,doc){
-           console.log(doc);
-
-           doc.on('remoteop',function(op){
-               console.log("json object update");
-               console.log(op)
-            });
-
-
+           $state.submitOp({p:[],od:null,oi:{x:x,y:y}});
        });
 
+
+      function stateUpdated(op) {
+        if (op) {
+          console.log(op);
+          console.log('here');
+        } else {
+
+        }
+        
+        
+      }
+
+       sharejs.open('dataDoc','json',function(error,doc){
+          window.$state = doc;
+          doc.on('change', function (op) {
+            stateUpdated(op);
+          });
+          
+          if (doc.created) 
+          {
+            var name = getURLParameter('name');
+            $localState = {};
+            $localState[name] = {x:0,y:0};
+            $localState = {p:[],od:null,oi:$localState};
+            //documentation on JSON properties at: https://github.com/ottypes/json0
+            doc.submitOp([{p:[],od:null,oi:$localState}])
+            console.log("created document");
+          } 
+          else 
+          {
+            stateUpdated();
+          }
+          
+       });  
    }
 );
